9ce1a0f14741d64690df74da6e22e734341ab90b
th3340 <th3340@k12.sd.us>
2020-02-04 12:44:53 -0600

    done

--- car.py
+++ car.py
@@ -29,4 +29,35 @@ class Car():
             print("You can't roll back an odometer!")
     def increment_odometer(self, miles):
         self.odometer_reading += miles
-
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+        if self.battery_size == 70:
+            self.battery_size = 85
+            print ('If you battery gets upgraded to aa 85-KWH battery, it could go 270 miles on a full charge.')
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+        """Initialize attributes of the parent class."""
+    def fill_gas_tank():
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't need a gas tank!")
